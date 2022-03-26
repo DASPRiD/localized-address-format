d9dd00e7304696c36845cb14d69add35b5dc84fa
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-03-26 20:18:35 +0100

    ci: exclude automatic release commit messages from linting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    ignores: [
+        commit => commit.startsWith('chore(release): set `package.json` to'),
+    ],
+};
